8009e0f7ce4480ed2b237db070b4d0d983ab7742
Nathan Cowley <nathancowley12@gmail.com>
2020-06-05 12:16:03 -0600

    testing all done. Onto questions

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -32,6 +32,7 @@ const CheckoutForm = (props) => {
         <label>
           First Name:
           <input
+            id="firstName"
             name="firstName"
             value={values.firstName}
             onChange={handleChanges}
@@ -40,6 +41,7 @@ const CheckoutForm = (props) => {
         <label>
           Last Name:
           <input
+            id="lastName"
             name="lastName"
             value={values.lastName}
             onChange={handleChanges}
@@ -48,6 +50,7 @@ const CheckoutForm = (props) => {
         <label>
           Address:
           <input
+            id="address"
             name="address"
             value={values.address}
             onChange={handleChanges}
@@ -55,17 +58,17 @@ const CheckoutForm = (props) => {
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input id="city" name="city" value={values.city} onChange={handleChanges} />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input id="state" name="state" value={values.state} onChange={handleChanges} />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input id="zip" name="zip" value={values.zip} onChange={handleChanges} />
         </label>
-        <button>Checkout</button>
+        <button data-testid="checkout">Checkout</button>
       </form>
 
       {showSuccessMessage && (
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,33 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, getByText } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const {getByText} = render(<CheckoutForm />)
+        getByText(/Checkout Form/i)
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    const {getByLabelText, getByTestId} = render(<CheckoutForm />);
+
+    const firstNameInput = getByLabelText(/first name/i)
+    const lastNameInput = getByLabelText(/last name/i)
+    const address = getByLabelText(/address/i)
+    const city = getByLabelText(/city/i)
+    const state = getByLabelText(/state/i)
+    const zip = getByLabelText(/zip/i)
+
+    fireEvent.change(firstNameInput, {target: {value: 'nate'}})
+    fireEvent.change(firstNameInput, {target: {value: 'cowley'}})
+    fireEvent.change(firstNameInput, {target: {value: '874 e 7493 n'}})
+    fireEvent.change(firstNameInput, {target: {value: 'Lehi'}})
+    fireEvent.change(firstNameInput, {target: {value: 'Utah'}})
+    fireEvent.change(firstNameInput, {target: {value: '84043'}})
+
+    const submitButton = getByTestId(/checkout/i)
+    fireEvent.click(submitButton);
+
+    const successMessage = getByTestId(/successMessage/i)
+});
